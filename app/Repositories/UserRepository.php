c548586129035f4b194f77519e56e48722d49aa8
mehdi1 <zougari@webpick.net>
2023-03-20 16:13:02 +0000

    [tr - wpk-training/2414] Media - Pre Embauche - PFE 2023 - UBO - Réalisation - Back - Réalisation des CRUD - Réalisation de CRUD de wp users, wp sites et wp roles

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -15,15 +15,33 @@ class UserRepository implements UserRepositoryInterface
     public function getAllUsers(){
         return User::paginate(10);
     }
-
+    
+    /**
+     * createUser
+     *
+     * @param  array $data
+     * @return User
+     */
     public function createUser(array $data): User{
         return User::create($data);
     }
-
+    
+    /**
+     * getUserByEmail
+     *
+     * @param  string $email
+     * @return User
+     */
     public function getUserByEmail(string $email): User{
         return  User::where('email', $email)->first();
     }
-
+    
+    /**
+     * getUserById
+     *
+     * @param  int $userId
+     * @return User
+     */
     public function getUserById (int $userId): User{
         return User::findOrFail($userId);
     }
