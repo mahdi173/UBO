62062f8f1bc5bc9d8cbb6df932b10cd55695eaf6
mehdi1 <zougari@webpick.net>
2023-04-04 08:11:21 +0000

    [tr - wpk-training/2608] Media - Pre Embauche - PFE 2023 - UBO - Réalisation - Back - Modules - WpRole - Creation des unit tests de crud

--- tests/Feature/CrudWpRoleTest.php
+++ tests/Feature/CrudWpRoleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,63 +11,90 @@ class CrudWpRoleTest extends TestCase
 {
     public function testGetAllWpRoles(): void
     {
-        $this->json('GET', 'api/wp-roles', ['Accept' => 'application/json'])
+        $token= $this->getUserToken();
+
+        $this->withHeader('Authorization', 'Bearer ' . $token)
+        ->json('GET', 'api/wp-roles', ['Accept' => 'application/json'])
         ->assertStatus(200);
     }
 
     public function testStoreWpRole(): void
     {
-        $data = [
+        $token= $this->getUserToken();
+
+        $role = [
             "name" => fake()->name
         ];
 
-        $this->json('POST', 'api/wp-roles', $data, ['Accept' => 'application/json'])
-        ->assertStatus(200)
-        ->assertJsonStructure([
-            "name",
-            "updated_at",
-            "created_at",
-            "id"
-        ]);
+        $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->json('POST', 'api/wp-roles', $role, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                "name",
+                "updated_at",
+                "created_at",
+                "id",
+            ]);
     }
 
     public function testUpdateWpRole(): void
     {
-        $data = [
+        $token= $this->getUserToken();
+
+        $role = [
             "name" => fake()->name
         ];
 
-        $this->json('PUT', 'api/wp-roles/11', $data, ['Accept' => 'application/json'])
-        ->assertStatus(200)
-        ->assertJsonStructure([
-            "id",
-            "name",
-            "deleted_at",
-            "updated_at",
-            "created_at",
-        ]);
+        $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->json('PUT', 'api/wp-roles/11', $role, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                "id",
+                "name",
+                "deleted_at",
+                "updated_at",
+                "created_at",
+            ]);
     }
 
     public function testDeleteRole(): void
-    {
+    {        
+        $token= $this->getUserToken();
+
         $id= rand(1,10);
-        $this->json('delete', 'api/wp-roles/'.$id, ['Accept' => 'application/json'])
-        ->assertStatus(200)
-        ->assertJsonStructure([
-            "msg"
-        ]);
+        $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->json('delete', 'api/wp-roles/'.$id, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                "msg"
+            ]);
     }
 
     public function testGetWpRole(): void
     {
-        $this->json('GET', 'api/wp-roles/6', ['Accept' => 'application/json'])
-        ->assertStatus(200)
-        ->assertJsonStructure([
-            "id",
-            "name",
-            "deleted_at",
-            "updated_at",
-            "created_at",
-        ]);
+        $token= $this->getUserToken();
+
+        $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->json('GET', 'api/wp-roles/6', ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                "id",
+                "name",
+                "deleted_at",
+                "updated_at",
+                "created_at",
+            ]);
+    }
+
+    public function getUserToken(){
+        $user=  User::factory()->create();
+        $loginData = [
+            "email" => $user->email,
+            "password" => "secret",
+        ];
+
+        $response= $this->json('POST', 'api/login', $loginData, ['Accept' => 'application/json']);
+        $data=json_decode($response->getContent());
+        return $data->token;
     }
 }
