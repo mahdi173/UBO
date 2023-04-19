a8b7d67a685b6a5451edfe51669c85d4b411c3f7
mehdi1 <zougari@webpick.net>
2023-04-19 11:36:14 +0000

    [tr wpk-training/2792] Media - Pre Embauche - PFE 2023 - UBO - Réalisation - Back - Modules - WpSite - Autorisations

--- app/Http/Controllers/WpSiteController.php
+++ app/Http/Controllers/WpSiteController.php
@@ -9,6 +9,7 @@ use App\Services\WpSiteService;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 class WpSiteController extends Controller
@@ -81,9 +82,9 @@ class WpSiteController extends Controller
      */
     public function destroy( WpSite $wpSite): JsonResponse
     {
-        if (Gate::denies('delete', $wpSite)) {
-            abort(403);
-        }  
+        if(Auth::user()->cannot('delete', WpSite::class)){
+            abort(403); 
+        } 
         return $this->wpSiteService->deleteWpSite($wpSite);
     }
 
