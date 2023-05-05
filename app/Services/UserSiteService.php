e82a03e510efe29c134a6f5c831bbfae9c223612
mehdi1 <zougari@webpick.net>
2023-05-05 15:56:06 +0100

    [tr wpk-training/2938] Media - Pre Embauche - PFE 2023 - UBO - Réalisation - Back - Modules - UserSite - Refactoring attach

--- app/Services/UserSiteService.php
+++ app/Services/UserSiteService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\UserSite;
 use App\Models\WpSite;
 use App\Models\WpUser;
 
@@ -10,7 +11,15 @@ class UserSiteService
     public function attach($id, mixed $target, array $data)
     {
         if($target instanceof WpUser){
-            $target->sites()->attach($id, $data);
+           $relation_exist= UserSite::where('wp_user_id', $target->id)
+                               ->where('wp_site_id', $id)->first();
+           
+            if($relation_exist){
+                UserSite::where('wp_user_id', $target->id)
+                ->where('wp_site_id', $id)->update($data);
+            }else{
+                $target->sites()->attach($id, $data);
+            }
         }else if($target instanceof WpSite){
             $target->users()->attach($id, $data);
         }
